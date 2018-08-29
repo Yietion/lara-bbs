f8e8e232585c940a4a64f1dd26a9ae00dd301fc6
Yietion <549166802@qq.com>
2018-08-29 16:21:09 +0000

    larabbs

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\User;
+
+// creating, created, updating, updated, saving,
+// saved,  deleting, deleted, restoring, restored
+
+class UserObserver
+{
+    public function saving(User $user)
+    {
+        if (empty($user->avatar)) {
+            $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/30/1/TrJS40Ey5k.png';
+        }
+    }
+    public function creating(User $user)
+    {
+        //
+    }
+
+    public function updating(User $user)
+    {
+        //
+    }
+}
---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Carbon\Carbon;
+use Laravel\Passport\Passport;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+		 \App\Models\Reply::class => \App\Policies\ReplyPolicy::class,
+		 \App\Models\Topic::class => \App\Policies\TopicPolicy::class,
+        'App\Model' => 'App\Policies\ModelPolicy',
+        \App\Models\User::class => \App\Policies\UserPolicy::class,
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        // Passport 的路由
+        Passport::routes();
+        // access_token 过期时间
+        Passport::tokensExpireIn(Carbon::now()->addDays(15));
+        // refreshTokens 过期时间
+        Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
+        //
+        \Horizon::auth(function($requiest){
+           return \Auth::user()->hasRole('Founder') ;
+        });
+    }
+}
